f8581418ba5e8965d3e5c5e8c49d241d506915c5
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2022-12-25 20:08:23 +0300

    Add lighting shader

--- src/samples/raytracing/simple_render.h
+++ src/samples/raytracing/simple_render.h
@@ -248,7 +248,7 @@ protected:
   VkBuffer appliedLightingBuffer = VK_NULL_HANDLE;
   VkDeviceMemory appliedLightingMem = VK_NULL_HANDLE;
   uint32_t trianglesCount = 0;
-  const float VOXEL_SIZE = 0.25f * 0.75;
+  const float VOXEL_SIZE = 0.5f;
   LiteMath::uint3 voxelsGrid;
   uint32_t voxelsCount = 0;
   uint32_t clustersCount = 0;
